c2e21b5145baa632c70a3ae6a8115ceecd320a63
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-09-18 20:59:19 -0300

    Falta editar las especificaciones de los caballos

---
+++ assets/js/modal.js
@@ -0,0 +1,196 @@
+const caballos = [
+    {
+        // 0
+        nombre: "Agatha",
+        genero: "Hembra",
+        fechaNacimiento: "29/04/1980",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Agatha/agatha.jpg" ]
+    }, {
+        // 1
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    }, {
+        // 2
+        nombre: "HLP-Maksum",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Maksum.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Maksum/HLP-Maksum_07.jpg" ]
+    } , {
+        // 3
+        nombre: "Angela",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Angela.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Angela/angela.jpg" ]
+    } , {
+        // 4
+        nombre: "Aston",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Aston/Aston.jpg" ]
+    } , {
+        // 5
+        nombre: "Charles",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Charles/Charles.jpg" ]
+    } , {
+        // 6
+        nombre: "HLP-Charlotte",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Charlotte/Charlotte_01.jpg" ]
+    } , {
+        // 7
+        nombre: "Dominiko",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Domeniko/domeniko01.jpg" ]
+    } , {
+        // 8
+        nombre: "Emily",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Emily/emily01.jpg" ]
+    } , {
+        // 9
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 10
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 11
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 12
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 13
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 14
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 15
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } , {
+        // 16
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg" ]
+    } 
+]
+
+function cargar(event){ 
+    const modal = document.querySelector(".modal");
+    const nombre = modal.querySelector(".modal-title");
+    const value = event.target.value;
+    const img = modal.querySelector("img");
+    const link = modal.querySelector(".documentacion");
+    const genero = modal.querySelector(".genero");
+    const fechaNacimiento = modal.querySelector(".fechaNacimiento");
+    const padre = modal.querySelector(".padre");
+    const madre = modal.querySelector(".madre");
+    const detalles = modal.querySelector(".detalles");
+    nombre.innerHTML = caballos[value].nombre;
+    genero.innerHTML = caballos[value].genero;
+    fechaNacimiento.innerHTML = caballos[value].fechaNacimiento;
+    padre.innerHTML = caballos[value].padre;
+    madre.innerHTML = caballos[value].madre;
+    detalles.innerHTML = caballos[value].detalles;
+    img.setAttribute("src",caballos[value].imagenes[0]);
+    link.setAttribute("href", caballos[value].documentacion);
+
+    console.log(event.target.value);
+}
