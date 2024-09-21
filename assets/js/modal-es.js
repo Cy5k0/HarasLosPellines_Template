3430fe4336bde326acd5e93550e2cbe95da15273
Francisco Monroy <83427525+fmonroy75@users.noreply.github.com>
2024-09-20 20:17:42 -0400

    version español

---
+++ assets/js/modal-es.js
@@ -0,0 +1,210 @@
+const caballos = [
+    {
+        // 0
+        nombre: "Agatha",
+        genero: "Hembra",
+        fechaNacimiento: "29/04/1980",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Agatha/agatha.jpg", "../assets/img/imgsins/Alma/HLP-Alma.jpg"]
+    }, {
+        // 1
+        nombre: "Alma",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Alma/HLP-Alma.jpg"]
+    }, {
+        // 2
+        nombre: "HLP-Maksum",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Maksum.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Maksum/HLP-Maksum_07.jpg"]
+    }, {
+        // 3
+        nombre: "Angela",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Angela.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Angela/angela.jpg"]
+    }, {
+        // 4
+        nombre: "Aston",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Aston/Aston.jpg"]
+    }, {
+        // 5
+        nombre: "Charles",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Charles/Charles.jpg"]
+    }, {
+        // 6
+        nombre: "HLP-Charlotte",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Charlotte/Charlotte_01.jpg"]
+    }, {
+        // 7
+        nombre: "Dominiko",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Domeniko/domeniko01.jpg"]
+    }, {
+        // 8
+        nombre: "Emily",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Emily.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Emily/emily01.jpg"]
+    }, {
+        // 9
+        nombre: "Johara",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Shangrila_Johara.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Johara/johara.jpg"],
+    }, {
+        // 10
+        nombre: "Martina",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Shangrila_Martina.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Martina/martina02.jpg"],
+    }, {
+        // 11
+        nombre: "Melika",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/HP_Melinka.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Melika/melika01.jpg"],
+    }, {
+        // 12
+        nombre: "Promessa",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Promessa.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Promessaa/promessaa02.jpg"],
+    }, {
+        // 13
+        nombre: "HLP-Sofía",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Sofia.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Sofía/HLP-Sofia_02.jpg"],
+    }, {
+        // 14
+        nombre: "Sultán",
+        genero: "Macho",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Sultan.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Sultan/sultan01.jpg"],
+    }, {
+        // 15
+        nombre: "Very Magical",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "Pendiente",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Very_Magical/very_magical04.jpg"],
+    }, {
+        // 16
+        nombre: "Yassira",
+        genero: "Hembra",
+        fechaNacimiento: "Pendiente",
+        padre: "Caballo XY",
+        madre: "Caballo XX",
+        documentacion: "./assets/doc/Yassira.pdf",
+        detalles: "Aquí puede ir algunos detalles como participacion en concursos exibiciones, etc....",
+        imagenes: ["../assets/img/imgsins/Yassira/yassira02.jpg"]
+    }
+]
+
+function cargar(event) {
+    const modal = document.querySelector(".modal");
+    const nombre = modal.querySelector(".modal-title");
+    const value = event.target.value;
+    const carousel = modal.querySelector(".carousel-inner")
+
+    const link = modal.querySelector(".documentacion");
+    const genero = modal.querySelector(".genero");
+    const fechaNacimiento = modal.querySelector(".fechaNacimiento");
+    const padre = modal.querySelector(".padre");
+    const madre = modal.querySelector(".madre");
+    const detalles = modal.querySelector(".detalles");
+    nombre.innerHTML = caballos[value].nombre;
+    genero.innerHTML = caballos[value].genero;
+    fechaNacimiento.innerHTML = caballos[value].fechaNacimiento;
+    padre.innerHTML = caballos[value].padre;
+    madre.innerHTML = caballos[value].madre;
+    detalles.innerHTML = caballos[value].detalles;
+    // img.setAttribute("src", caballos[value].imagenes[0]);
+    link.setAttribute("href", caballos[value].documentacion);
+
+    // REINICIO CAROUSEL
+    carousel.innerHTML="";
+    caballos[value].imagenes.forEach(imagen => {
+
+        carousel.innerHTML += `
+            <div class="carousel-item">
+                <img src="${imagen}" class="d-block w-100" alt="..." >
+            </div>
+        `
+    })
+    const img = modal.querySelector(".carousel-item");
+    img.classList.add("active");
+
+    console.log(event.target.value);
+}
