c5d7ff3d79d616ad947906fd27921a1f81fade93
Francisco Monroy <83427525+fmonroy75@users.noreply.github.com>
2024-09-23 15:45:00 -0400

    contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,7 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'haras.los.pellines@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -34,6 +22,7 @@
   */
 
   $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['subject'], 'Subject');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
